70bd3eff94c7aca704a647a29a4a40beff9953dd
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2023-02-03 22:03:31 +0530

    Feat:ImageUploader serving to homePage

---
+++ components/imageUploader.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Loader from './Loader';
+
+// Uploads images to Firebase Storage
+export default function ImageUploader() {
+    const [uploading, setUploading] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [downloadURL, setDownloadURL] = useState(null);
+
+    return (
+        <div className="box">
+            <Loader show={uploading} />
+            {uploading && <h3>{progress}%</h3>}
+
+            {!uploading && (
+                <>
+                    <label className="btn">
+                        📸 Upload Img
+                        
+                    </label>
+                </>
+            )}
+
+            {downloadURL && <code className="upload-snippet">{`![alt](${downloadURL})`}</code>}
+        </div>
+    );
+}
